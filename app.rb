6b40d3137690da99244455527a2d2750fe9f9533
Hayden Betts <haydenbetts@gmail.com>
2017-09-16 22:16:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ class App < Sinatra::Base
   post '/' do
     text_from_user = params[:user_text]
 
+    # right we don't just pass data into instance variables
+    # so we can use them in view, but also whole object instances
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+
     erb :results
   end
 end
